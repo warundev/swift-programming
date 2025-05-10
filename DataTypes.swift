5ff604a2fa6d501ee984a5b52b30eab414ab568a
warundev <120333797+warundev@users.noreply.github.com>
2025-05-11 01:39:17 +0530

    swift basic

---
+++ DataTypes.swift
@@ -0,0 +1,9 @@
+var age: Int = 22
+var score = 95.5       // Double
+var isPassed = true    // Bool
+
+
+print(age)
+print(score)
+print(isPassed)
+
---
+++ Variables.swift
@@ -0,0 +1,3 @@
+let pi = 3.14        // constant
+var name = "Waruna"  // variable
+print("Hello, \(name), Pi = \(pi)")
